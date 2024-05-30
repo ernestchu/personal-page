4bc31d816255212c17279b1cbb7eee7101be94cd
ernestchu <shchu@citi.sinica.edu.tw>
2024-05-30 18:13:09 +0800

    add GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,17 @@ import ViteYaml from "@modyfi/vite-plugin-yaml"
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     buildAssetsDir: 'custom_assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
+
   vite: {
     plugins: [ViteYaml()],
+  },
+
+  modules: ["nuxt-gtag"],
+  gtag: {
+    id: 'G-X4R9WJR7YV',
   },
 })
